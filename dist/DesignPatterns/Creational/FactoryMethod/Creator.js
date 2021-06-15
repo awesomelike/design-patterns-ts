ed524a353860c5c022e0702d77c35c5cc09c3e1d
awesomelike <abdullaev.dev@gmail.com>
2021-06-15 17:49:31 +0500

    add factory method

--- dist/DesignPatterns/Creational/FactoryMethod/Creator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Creator {
-    someOperation() {
-        const product = this.createProduct();
-        return `Creator: The same creator's code worked for ${product.doOperation()}`;
-    }
-}
-exports.default = Creator;
